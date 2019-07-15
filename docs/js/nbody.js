fdb94a948ab9607aab95f7219ebb2e12488b7f2b
sksat <sksat@sksat.net>
2019-07-15 23:58:08 +0900

    [add] nbody wasm

--- docs/js/nbody.js
+++ docs/js/nbody.js
@@ -1,9 +1,14 @@
+var wasm = {}; // WebAssembly
+
+// canvas
 var canvas = document.getElementById("canvas");
 if(!canvas || ! canvas.getContext){
 	alert("error: canvasが使えません");
+}else{
+	console.log("[ok] canvas support");
 }
-var context= canvas.getContext('2d');
 
+// 描画系関数
 function fill_back(){
 	var context = canvas.getContext('2d');
 	context.beginPath();
@@ -19,4 +24,24 @@ function draw_circle(x, y, r){
 	context.fill();
 }
 
+// 関数をimportできるようにここでwasmを読み込む
+const objs = {
+	imports: {
+		draw_circle: function(arg){
+			console.log(arg);
+		}
+	}
+}; // importよくわからん
+load_wasm("wasm/nbody.wasm")
+	.then(instance => {
+		wasm = instance;
+		var exports = instance.exports;
+	});
+
+// イベントハンドラ
+function on_start(){
+	console.log(wasm.exports.add(100,4));
+}
+
 fill_back(); // とりあえず塗りつぶしておく
+draw_circle(100, 100, 50);
---
+++ src/nbody.c
@@ -0,0 +1,6 @@
+// imported func
+//void draw_circle(int x, int y, int r);
+
+void draw(){
+//	draw_circle(150, 150, 40);
+}
